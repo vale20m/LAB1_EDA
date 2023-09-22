6d3fc0def950f2eaad5febf57d7c744bb4bc2852
Valentín Veintemilla <valentinveintemilla@gmail.com>
2023-09-22 15:05:58 -0300

    Laboratorio completado

--- LAB1_EDA.cpp
+++ LAB1_EDA.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #define MAX_CHAR 3
 #define TEXTO_MAX 22
 using namespace std;
@@ -16,8 +17,8 @@ struct archivo{
     Cadena extension;
     bool escritura = true;
     string contenido;
-    archivo * sig = NULL;
-    archivo * ant = NULL;
+    archivo * sig;
+    archivo * ant;
 };
 
 typedef archivo * archivos;
@@ -32,9 +33,8 @@ struct directorio {
 typedef directorio * dir;
 
 struct _sistema{
-    string nombre;
+    string nombre = "SISTEMA";
     dir RAIZ;
-    dir RAIZptr;
 };
 
 typedef _sistema* Sistema;
@@ -47,12 +47,20 @@ Sistema MAIN = NULL;
 
 // CREATEFILE
 
-bool existeArch(Sistema s, Cadena nombre, Cadena extension){
-    while(s->RAIZ->file->nombre != ""){
-        if(s->RAIZ->file->nombre == nombre && s->RAIZ->file->extension == extension){
+void moverPunteroAlFinal(archivos & aux, Sistema s){
+    aux = s->RAIZ->file;
+    while(aux->sig != NULL){
+        aux = aux->sig;
+    }
+}
+
+bool existeArch(archivos & aux, Sistema s, Cadena nombre, Cadena extension){
+    aux = s->RAIZ->file;
+    while(aux != NULL){
+        if(aux->nombre == nombre && aux->extension == extension){
             return true;
         }
-        s->RAIZ->file = s->RAIZ->file->sig;
+        aux = aux->sig;
     }
     return false;
 }
@@ -82,30 +90,19 @@ Cadena tomarExtension(Cadena nombreArchivo){
     }
 }
 
-archivos colocarPunteroAlFinal(Sistema s){
-    archivos aux = NULL;
-    while(s->RAIZ->file->nombre != ""){
-        s->RAIZ->file = s->RAIZ->file->sig;
-    }
-    aux = s->RAIZ->file;
-    return aux;
-}
-
 TipoRet CREATEFILE (Sistema & s, Cadena nombreArchivo){
-    if(!existeArch(s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo)) && nombreArchivo != "RAIZ"){
-        archivos aux = colocarPunteroAlFinal(s);
+    archivos puntVerificar = NULL;
+    if(!existeArch(puntVerificar, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo)) && nombreArchivo != "RAIZ"){
 
-        aux->nombre = tomarNombre(nombreArchivo);
-        cout << aux->nombre << endl;
+        archivos puntFinal = NULL;
+        moverPunteroAlFinal(puntFinal, s);
 
-        aux->extension = tomarExtension(nombreArchivo);
-        cout << aux->extension << endl;
+        archivos nuevoArchivo = new archivo;
+        nuevoArchivo->nombre = tomarNombre(nombreArchivo);
+        nuevoArchivo->extension = tomarExtension(nombreArchivo);
+        nuevoArchivo->sig = NULL;
 
-        aux->sig = new archivo;
-        aux->sig->nombre = "";
-        aux->sig->extension = "";
-        
-        cout << "Salí de createfile" << endl;
+        puntFinal->sig = nuevoArchivo;
         return OK;
     } else {
         cout << "Ya existe un archivo con ese nombre completo en el directorio actual" << endl;
@@ -116,31 +113,115 @@ TipoRet CREATEFILE (Sistema & s, Cadena nombreArchivo){
 // DELETE
 
 TipoRet DELETE (Sistema & s, Cadena nombreArchivo){
-
+    archivos borrar = NULL;
+    if(existeArch(borrar, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo))){
+        if(!borrar->escritura){
+            cout << "El archivo existe, pero es de sólo lectura." << endl;
+            return ERROR;
+        }
+        archivos aux = s->RAIZ->file;
+        while(aux->sig != borrar){
+            aux = aux->sig;
+        }
+        aux->sig = borrar->sig;
+        delete borrar;
+        return OK;
+    } else {
+        cout << "No existe un archivo con ese nombre en el directorio actual" << endl;
+        return ERROR;
+    }
 }
 
 // ATTRIB
 
 TipoRet ATTRIB (Sistema & s, Cadena nombreArchivo, Cadena parametro){
-
+    archivos aux = NULL;
+    if(existeArch(aux, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo))){
+        if(parametro == "+W"){
+            aux->escritura = true;
+            return OK;
+        } else {
+            aux->escritura = false;
+            return OK;
+        }
+    } else {
+        cout << "No existe un archivo con ese nombre en el directorio actual" << endl;
+        return ERROR;
+    }
 }
 
 // IF
 
-TipoRet IF (Sistema & s, Cadena nombreArchivo, Cadena texto){
+void agregarTexto(archivos aux, Sistema s, Cadena texto){
+    if (texto.length() >= TEXTO_MAX){
+        aux->contenido = "";
+        for (int i = 0; i < TEXTO_MAX; i++){
+            aux->contenido += texto[i];
+        }
+    } else {
+        Cadena nuevoTexto = "";
+        for (int i = 0; i < texto.length(); i++){
+            nuevoTexto += texto[i];
+        }
+        for (int i = 0; i < (TEXTO_MAX - texto.length()); i++){
+            nuevoTexto += aux->contenido[i];
+        }
+        aux->contenido = nuevoTexto;
+    }
+}
 
+TipoRet IF (Sistema & s, Cadena nombreArchivo, Cadena texto){
+    archivos aux = NULL;
+    if(existeArch(aux, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo))){
+        if (!aux->escritura){
+            cout << "El archivo es de sólo lectura" << endl;
+            return ERROR;
+        }
+        agregarTexto(aux, s, texto);
+        return OK;
+    } else {
+        cout << "No existe un archivo con ese nombre en el directorio actual" << endl;
+        return ERROR;
+    }
 }
 
 // DF
 
 TipoRet DF (Sistema & s, Cadena nombreArchivo, int k){
-
+    archivos aux = NULL;
+    if(existeArch(aux, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo))){
+        if (!aux->escritura){
+            cout << "El archivo es de sólo lectura" << endl;
+            return ERROR;
+        }
+        Cadena resultado = "";
+        for(int i = k; i < aux->contenido.length(); i++){
+            resultado += aux->contenido[i];
+        }
+        cout << resultado << endl;
+        aux->contenido = resultado;
+        return OK;
+    } else {
+        cout << "No existe un archivo con ese nombre en el directorio actual" << endl;
+        return ERROR;
+    }
 }
 
 // TYPE
 
 TipoRet TYPE (Sistema & s, Cadena nombreArchivo){
-
+    archivos aux = NULL;
+    if(existeArch(aux, s, tomarNombre(nombreArchivo), tomarExtension(nombreArchivo))){
+        if (aux->contenido == ""){
+            cout << "El archivo parámetro no posee contenido" << endl;
+            return OK;
+        }
+        cout << aux->contenido << endl;
+        return OK;
+    } else {
+        cout << "No existe un archivo con ese nombre en el directorio actual" << endl;
+        return ERROR;
+    }
 }
 
 // CREARSISTEMA
@@ -149,18 +230,27 @@ TipoRet CREARSISTEMA(Sistema & s){
     s = new _sistema;
     s->RAIZ = new directorio;
     s->RAIZ->nombre = "RAIZ";
-    s->RAIZptr = s->RAIZ;
     s->RAIZ->file = new archivo;
-    s->RAIZ->file->nombre = "";
-    s->RAIZ->file->extension = "";
-    cout << "Se ha creado exitosamente" << endl;
+    s->RAIZ->file->nombre = "NULL";
+    s->RAIZ->file->sig = NULL;
     return OK;
 }
 
 // DESTRUIRSISTEMA
 
-TipoRet DESTRUIRSISTEMA(Sistema & s){
+void borrarArchivos(archivos borrar){
+    if (borrar->sig != NULL){
+        borrarArchivos(borrar->sig);
+    }
+    delete borrar;
+}
 
+TipoRet DESTRUIRSISTEMA(Sistema & s){
+    archivos borrar = s->RAIZ->file;
+    borrarArchivos(borrar);
+    delete s->RAIZ;
+    delete s;
+    return OK;
 }
 
 // ANÁLISIS DE COMANDO
@@ -168,25 +258,39 @@ TipoRet DESTRUIRSISTEMA(Sistema & s){
 void dividirComando(Cadena comandoEntero, int & cont, Cadena & Cambio){
     Cambio = "";
     while(comandoEntero[cont] != ' ' && cont < comandoEntero.length()){
+        if(comandoEntero[cont] == '"'){
+            cont++;
+            while(cont < (comandoEntero.length()-1)){
+                Cambio += comandoEntero[cont];
+                cont++;
+            }
+            return;
+        }
         Cambio += comandoEntero[cont];
         cont++;
     }
     cont++;
 }
 
+int convertirAInt(Cadena parametro){
+    int numero = 0;
+    for(int i = 0; i < parametro.length(); i++){
+        numero += (parametro[i]-48)*pow(10, (parametro.length()-(1+i)));
+    }
+    return numero;
+}
+
 void analizarComando(Cadena comando, Cadena parametro1, Cadena parametro2){
     int cont = 0;
-    cout << comando << endl;
-    cout << parametro1 << endl;
     if (comando == "CREARSISTEMA") {
         if(parametro1 == "" && parametro2 == ""){
             CREARSISTEMA(MAIN);
-            cout << "Salí de crear sistema" << endl;
         } else {
             return;
         }
     }
     if(comando == "DESTRUIRSISTEMA"){
+        DESTRUIRSISTEMA(MAIN);
         return;
     }
     if (comando == "CREATEFILE") {
@@ -194,18 +298,23 @@ void analizarComando(Cadena comando, Cadena parametro1, Cadena parametro2){
         return;
     }
     if (comando == "DELETE") {
+        DELETE(MAIN, parametro1);
         return;
     }
     if (comando == "ATTRIB") {
+        ATTRIB(MAIN, parametro1, parametro2);
         return;
     }
     if (comando == "IF") {
+        IF(MAIN, parametro1, parametro2);
         return;
     }
     if (comando == "DF") {
+        DF(MAIN, parametro1, convertirAInt(parametro2));
         return;
     }
     if (comando == "TYPE") {
+        TYPE(MAIN, parametro1);
         return;
     }
 }
@@ -230,29 +339,19 @@ void manejarComando(){
 
 // MAIN
 
+// leerExistentes sirve para verificar que el código esté funcionando como es debido
+
+void leerExistentes(Sistema s){
+    cout << s->nombre << endl;
+    cout << s->RAIZ->nombre << endl;
+    while (s->RAIZ->file != NULL){
+        cout << s->RAIZ->file->nombre << endl;
+        cout << s->RAIZ->file->contenido << endl;
+        s->RAIZ->file = s->RAIZ->file->sig;
+    }
+}
+
 int main(){
     manejarComando();
-    cout << MAIN->RAIZ->file->nombre << endl;
-    cout << MAIN->RAIZ->file->sig->nombre << endl;
+    // leerExistentes(MAIN);
 }
-
-/*
-if (nombre == "RAIZ"){
-    cout << "No se puede crear un directorio con el nombre 'RAIZ'";
-    return ERROR;
-}
-*/
-
-/*
-if ()
-*/
-
-/* CREAR UN DIRECTORIO "RAIZ" EN SISTEMA
-    _sistema sistema1;
-    sistema1.RAIZ;
-    sistema1.RAIZ.nombre = "JIJIJIJA";
-    cout << sistema1.RAIZ.nombre;
-    sistema1.RAIZ.sig = new root;
-    sistema1.RAIZ.sig.nombre = "ELBANA";
-    cout << sistema1.RAIZ->sig.nombre;
-*/
